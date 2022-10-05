78ae85acb82fd4274518ccee873cd23a22e2982d
amanpreetnestormind <amanpreet.singh@nestormind.com>
2022-10-05 13:05:14 +0530

    code refactor

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const router = express.Router()
-const AppRoutes = require('../app_routes')
-const cors = require('cors')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const { mongo: { uri } } = require('./services/vars')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const router = express.Router();
+const AppRoutes = require("../app_routes");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const {
+  mongo: { uri },
+} = require("./services/vars");
+require("dotenv").config();
 
-mongoose.connect(uri)
-    .then(res => {
-        console.log(`Mongoose connection established ...`);
-    })
-    .catch(err => {
-        console.log(`Mongoose connection error ${err}`);
-    })
+mongoose
+  .connect(uri)
+  .then((res) => {
+    console.log(`Mongoose connection established ...`);
+  })
+  .catch((err) => {
+    console.log(`Mongoose connection error ${err}`);
+  });
 
 const corsOptions = {
-    origin: "*",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
 
 app.set("view engine", "ejs");
@@ -30,30 +33,42 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow_Origin", "*");
+  res.header("Access-Control-Allow_Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method == "OPTIONS") {
     res.header(
-        "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+      "Access-Control-Allow-Methods",
+      "PUT",
+      "POST",
+      "PATCH",
+      "DELETE",
+      "GET"
     );
-    if (req.method == "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "PUT",
-            "POST",
-            "PATCH",
-            "DELETE",
-            "GET"
-        );
-        return res.status(200).json({});
-    }
-    next();
+    return res.status(200).json({});
+  }
+  next();
 });
 
-app.options("*", cors({ origin: 'http://localhost:8080', optionsSuccessStatus: 200 }));
+app.options(
+  "*",
+  cors({ origin: "http://localhost:8080", optionsSuccessStatus: 200 })
+);
 
-app.use(cors({ origin: "http://localhost:8080", optionsSuccessStatus: 200 }))
+app.use(cors({ origin: "http://localhost:8080", optionsSuccessStatus: 200 }));
 
 // api router
-app.use('/api/v1', AppRoutes)
 
-module.exports = app
+app.get("/", async (req, res, next) => {
+  try {
+    res.status(200).json({
+      request: "get request",
+      message: "Connected successfully",
+    });
+  } catch (error) {}
+});
+app.use("/api/v1", AppRoutes);
+
+module.exports = app;
