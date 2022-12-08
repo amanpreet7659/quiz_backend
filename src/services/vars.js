2d633776ef2a9dd0228f3a066832092fba6ca818
amanpreetnestormind <amanpreet.singh@nestormind.com>
2022-12-08 18:33:22 +0530

    updated code

--- src/services/vars.js
+++ src/services/vars.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 module.exports = {
   env: process.env.NODE_ENV || "development",
   logs: process.env.NODE_ENV === "production" ? "combined" : "dev",
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 8484,
   mongo: {
-    uri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
+    uri:
+      process.env.MONGO_URI ||
+      "mongodb+srv://node_js_demo:GP1fvZncCa6CvzPJ@cluster0.xps2l.mongodb.net/quiz_app?retryWrites=true&w=majority",
   },
   jwt: {
     secret: process.env.JWT_SECRET || "0123456789",
